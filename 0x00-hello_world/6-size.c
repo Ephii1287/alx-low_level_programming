53bfadcf8e595a02dd4d172d7ecaef5774d36fa0
Ephii1287 <ephremgirma0@gmail.com>
2021-06-21 11:42:22 +0000

    modified 6th task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,10 +16,10 @@ long int longinttype;
 long long int longlonginttype;
 float floattype;
 
-printf("size of a char: %d byte(s)\n", sizeof(chartype));
-printf("size of an int: %d byte(s)\n", sizeof(inttype));
-printf("size of a long int: %d byte(s)\n", sizeof(longinttype));
-printf("size of a long long int: %d byte(s)\n", sizeof(longlonginttype));
-printf("size of a float: %d byte(s)\n", sizeof(floattype));
+printf("Size of a char: %d byte(s)\n", sizeof(chartype));
+printf("Size of an int: %d byte(s)\n", sizeof(inttype));
+printf("Size of a long int: %d byte(s)\n", sizeof(longinttype));
+printf("Size of a long long int: %d byte(s)\n", sizeof(longlonginttype));
+printf("Size of a float: %d byte(s)\n", sizeof(floattype));
 return (0);
 }
