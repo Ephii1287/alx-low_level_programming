0ea54fbe9b0c6586454e92ec9938eee22dff7197
Ephii1287 <ephremgirma0@gmail.com>
2021-06-23 12:34:50 +0000

    modified taks 4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _isalpha - Checks for alphabet character from a given input
+ *
+ * Return: Always 1 for lowercase character. 0 for the other
+ * @c: The character in ASCII code
+ *
+ */
+int _isalpha (int c)
+{
+  if (c >= 97 && c <=122)
+    {
+      if ( c >= 65 && c <= 90)
+	{
+	  return (1);
+	}
+    }
+  else
+    {
+      return (0);
+    }
+_putchar('\n');
+}
+
+    
