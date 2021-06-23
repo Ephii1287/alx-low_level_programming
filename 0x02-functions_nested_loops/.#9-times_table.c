36576e0220391ab9d3d254d84c2fbbf8fbf67dd4
Ephii1287 <ephremgirma0@gmail.com>
2021-06-23 18:44:21 +0000

    function that adds two numbers

--- 0x02-functions_nested_loops/.#9-times_table.c
+++
@@ -1,1 +0,0 @@
-vagrant@ubuntu-focal.64155:1624268401
--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,7 +4,7 @@
 * add - Adds two integers and returns the result
 * Return: integer
 */
-int add(int a, int b);
+int add(int a, int b)
 {
 return (a + b);
 }
