98aa83d5774f8660a3a540593a0f156f18ad73f5
Ephii1287 <ephremgirma0@gmail.com>
2021-06-23 12:00:04 +0000

    checks for lowercase letter

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _islower - Checks if the character is lowercase 
+ * prints 1 if lowercase or 0 if not
+ * Return: 0 always
+ */
+
+int _islower(int c)
+{
+  if (c >= 97 && c <= 122)
+    {
+      return (1);
+    }
+
+  else
+    {
+      retutn (0);
+    }
+  _putchar('\n);
+}
