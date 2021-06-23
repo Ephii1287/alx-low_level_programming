5ce5718992697606a7f403fedb59d38aa660fe9c
Ephii1287 <ephremgirma0@gmail.com>
2021-06-23 12:43:16 +0000

    modified taks 4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,17 +2,16 @@
 /**
  * _isalpha - Checks for alphabet character from a given input
  *
- * Return: Always 1 for lowercase character. 0 for the other
+ * Return: 1 for lowercase character. 0 for the other
  * @c: The character in ASCII code
  *
  */
-int _isalpha (int c)
+int _isalpha(int c)
 {
   if ((c >= 97 && c <=122) || (c >= 65 && c < = 90))
 	{
 	  return (1);
 	}
-    }
   else
     {
       return (0);
