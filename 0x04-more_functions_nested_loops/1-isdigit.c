b2ceb37e8a057b11338a7a03ce19e67800e57960
Ephii1287 <ephremgirma0@gmail.com>
2021-06-24 08:11:04 +0000

    created task 01 isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+* _isdigit - Shows 1 if the input is a
+* digitotherwise, shows
+* 0
+*
+* @c: Character in ASCII code
+*
+* Return: 1 for digit, 0 for others
+*/
+int _isdigit(int c)
+{
+if (n >= 48 && n <= 57)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
