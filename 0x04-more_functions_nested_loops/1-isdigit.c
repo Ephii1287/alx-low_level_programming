cf44085d3f0ad193a4283591dd6c037e4a371ff6
Ephii1287 <ephremgirma0@gmail.com>
2021-06-24 12:44:28 +0000

    modified task 1 is digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,21 +1,18 @@
 #include "holberton.h"
 
 /**
-* _isdigit - Shows 1 if the input is a
-* digitotherwise, shows
-* 0
-*
-* @c: Character in ASCII code
-*
-* Return: 1 for digit, 0 for others
-*/
+ * _isdigit - Checks if input is  between 0 to 9.
+ * @c: input number.
+ * Return: 1 if is a number (0 to 9), 0 in other case.
+ */
 int _isdigit(int c)
 {
-if (c >= 48 && c <= 57)
-{
-return (1);
-}
-else
-{
-return (0);
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
