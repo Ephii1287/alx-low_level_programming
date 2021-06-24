c207336b6da6bbe4b803ce39d56601336c7bb364
Ephii1287 <ephremgirma0@gmail.com>
2021-06-24 16:10:52 +0000

    modified task 5

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -7,6 +7,7 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
